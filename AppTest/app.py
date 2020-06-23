d62f1af1ba3e1bc10c46884adb60c1fb021cd2e2
celinmay <62461350+celinmay@users.noreply.github.com>
2020-06-23 17:10:24 +0200

    Add files via upload

--- AppTest/app.py
+++ AppTest/app.py
@@ -14,6 +14,8 @@ app.config['MONGO_URI'] = 'mongodb+srv://environmental:environmental@cluster0-jq
  #collection name
 mongo = PyMongo(app)
 
+england = [ ]
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -37,7 +39,7 @@ def germany():
     for q in countries.find():
         germany01.append(q['value'])
     print(q)
-    return render_template('germany.html', listgermany=germany01)
+    return render_template('germany.html', len = len(germany01), listgermany=germany01)
 
 @app.route('/england', methods=["GET"])
 def england(): 
@@ -57,6 +59,59 @@ def denmark():
     print(q)
     return render_template('denmark.html', listdenmark=denmark01)
 
+@app.route('/france', methods=["GET"])
+def france(): 
+        
+    france01 = []
+    for q in countries.find():
+        france01.append(q['value'])
+    print(q)
+    return render_template('france.html', listfrance=france01)
+
+@app.route('/spain', methods=["GET"])
+def spain(): 
+        
+    spain01 = []
+    for q in countries.find():
+        spain01.append(q['value'])
+    print(q)
+    return render_template('spain.html', listspain=spain01)
+
+@app.route('/poland', methods=["GET"])
+def poland(): 
+        
+    poland01 = []
+    for q in countries.find():
+        poland01.append(q['value'])
+    print(q)
+    return render_template('poland.html', listpoland=poland01)
+
+@app.route('/italy', methods=["GET"])
+def italy(): 
+        
+    italy01 = []
+    for q in countries.find():
+        italy01.append(q['value'])
+    print(q)
+    return render_template('italy.html', listitaly=italy01)
+
+@app.route('/russia', methods=["GET"])
+def russia(): 
+        
+    russia01 = []
+    for q in countries.find():
+        russia01.append(q['value'])
+    print(q)
+    return render_template('russia.html', listrussia=russia01)
+
+@app.route('/greece', methods=["GET"])
+def greece(): 
+        
+    greece01 = []
+    for q in countries.find():
+        greece01.append(q['value'])
+    print(q)
+    return render_template('greece.html', listgreece=greece01)
 #@app.route('/about', methods=['GET'])
 # def get_all_frameworks():
 #    return render_template('/about.html', name = "Celin")
